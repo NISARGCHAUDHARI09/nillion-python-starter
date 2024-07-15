6caa6a8b2294cac3377f5fb481473b7d82b34041
NISARGCHAUDHARI09 <nk5460@srmist.edu.in>
2024-07-15 14:30:28 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -1,12 +1,39 @@
 from nada_dsl import *
-def nada_main():
 
+def is_prime(n):
+    """Check if a number is prime."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def nada_main():
+    # Define the party
     party1 = Party(name="Party1")
 
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    # Define the secret integer inputs
+    secret_integers = [
+        SecretInteger(Input(name=f"my_int{i}", party=party1)) for i in range(5)
+    ]
 
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    # Find the maximum value
+    max_int = secret_integers[0]
+    for secret_int in secret_integers[1:]:
+        max_int = Select(secret_int > max_int, secret_int, max_int)
 
-    new_int = my_int1 + my_int2
+    # Check if the maximum value is a prime number
+    is_max_prime = is_prime(max_int.reveal())
 
-    return [Output(new_int, "my_output", party1)]
+    # Return the outputs
+    return [
+        Output(max_int, "max_value", party=party1),
+        Output(is_max_prime, "is_max_prime", party=party1)
+    ]
